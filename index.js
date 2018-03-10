2344e1b79a448d81cc7a93659f425bca2a8e9a40
trevinomatt <mattrtrevino@gmail.com>
2018-03-09 19:02:09 -0600

    Install mongoose & connect MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose_connect = require('mongoose');
+const config = require('./config/key');
+
+mongoose_connect.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log("MongoDB connect success")).catch(err => console.log(err))
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
