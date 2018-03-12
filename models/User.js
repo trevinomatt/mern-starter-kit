1286caabeda1ea7c6e957dddf47e5936fd3d5879
trevinomatt <mattrtrevino@gmail.com>
2018-03-12 04:52:16 -0500

    password encrypt with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -29,6 +31,30 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
+})
+
+userSchema.pre('save', function( next ){
+    let user = this;
+
+    if (user.isModified('password')) {
+        console.log('user isModified');
+        // password bcrypt
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) {
+                return next(err);
+            } else {
+                bcrypt.hash(user.password, salt, function(err, hash) {
+                    if (err) {
+                        return next(err);
+                    } else {
+                        user.password = hash;
+                        next();
+                    }
+                })
+            }
+        });
+    }
+
 })
 
 const User = mongoose.model('User', userSchema)
