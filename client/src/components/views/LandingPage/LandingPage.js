447f5e6ccabdaba044ebe9645b94fb3b69164aec
trevinomatt <mattrtrevino@gmail.com>
2018-03-26 06:16:24 -0500

    logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,33 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => console.log(response.data))
     }, [])
 
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push('/login');
+                } else {
+                    alert('failed logout');
+                }
+            });
+    }
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
-            LandingPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
+        }}>
+            <h2>LandingPage</h2>
+
+            <button onClick={onClickHandler}>
+                Logout
+            </button>
         </div>
     )
 }
